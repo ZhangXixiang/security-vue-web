2f1921aee1c67964d4c6e3b341f576a70fcb6118
xx <xx@xxdeMac-mini.local>
2019-07-12 14:07:33 +0800

    add upload moudle

--- ispace-api/src/main/java/com/space/config/CorsConfig.java
+++ ispace-api/src/main/java/com/space/config/CorsConfig.java
@@ -1,27 +1,15 @@
 package com.space.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfig {
-
-	private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
-        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
-        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
-        return corsConfiguration;
+@EnableWebMvc
+public class CorsConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig()); // 4
-        return new CorsFilter(source);
-    }
-	
 }
---
+++ ispace-common/src/main/java/com/space/common/util/BaseFileUtil.java
@@ -0,0 +1,104 @@
+package com.space.common.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * 文件处理工具类
+ */
+public class BaseFileUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseFileUtil.class);
+
+    /**
+     *  上传文件到服务器
+     * @param bt
+     * @param rootPath  静态资源目录
+     * @param filePath  文件目录
+     * @param fileName  文件名称
+     * @return
+     */
+    public static UploadResult writeFileToService(byte[] bt, String rootPath, String filePath, String fileName) {
+
+        BufferedOutputStream bos = null;
+        FileOutputStream fos = null;
+        File targetFile = null;
+
+        try {
+
+            File dir = new File(rootPath,filePath);
+
+            System.out.println(dir);
+
+            // 判断文件目录是否存在
+            if(!dir.exists()){
+                // 设置可写权限
+                dir.setWritable(true);
+                // 创建目录
+                dir.mkdirs();
+            }
+
+            //保存
+            targetFile = new File(dir, fileName);
+            fos = new FileOutputStream(targetFile);
+            bos = new BufferedOutputStream(fos);
+            bos.write(bt);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+
+        return new UploadResult(true, targetFile.getName());
+    }
+
+    public static UploadResult writeFileToService(MultipartFile multipartFile, String rootPath, String filePath){
+        String uploadFileName = multipartFile.getOriginalFilename();
+        //扩展名
+        //abc.jpg
+//        String fileExtensionName = fileName.substring(fileName.lastIndexOf(".")+1);
+//        String uploadFileName = UUID.randomUUID().toString()+"."+fileExtensionName;
+
+        File fileDir = new File(rootPath, filePath);
+        if(!fileDir.exists()){
+            // 设置可写权限
+            fileDir.setWritable(true);
+            // 创建目录
+            fileDir.mkdirs();
+        }
+
+        File targetFile = new File(fileDir, uploadFileName);
+
+        try {
+            multipartFile.transferTo(targetFile);
+            // 文件上传到文件夹成功了
+        } catch (IOException e) {
+            LOGGER.error("上传文件异常", e);
+            return null;
+        }
+
+        return new UploadResult(true, targetFile.getName());
+    }
+
+}
